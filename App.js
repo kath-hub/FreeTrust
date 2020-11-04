5877660d415a326c461295538401351e63464024
Jacky <jackytse0730@gmail.com>
2020-11-05 02:38:51 +0800

    firebase setup done

--- App.js
+++ App.js
@@ -10,24 +10,13 @@ export default class App extends React.Component {
 
   constructor(props){
     super(props);
-    this.state = {
-      isLoadingComplete: false,
-      }
+
     if (!firebase.apps.length) { firebase.initializeApp(ApiKeys.FirebaseConfig); }
 
     
   }
 
- render() {
-    if (!this.state.isLoadingComplete && !this.props.skipLoadingScreen) {
-      return (
-        <AppLoading
-          startAsync={this._loadResourcesAsync}
-          onError={this._handleLoadingError}
-          onFinish={this._handleFinishLoading}
-        />
-      );
-    } else {
+  render() {
       return (
         <View style={styles.container}>
           {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
@@ -35,7 +24,6 @@ export default class App extends React.Component {
           <SearchPage />
         </View>
       );
-    }
   }
 }
 
--- pages/SearchPage.js
+++ pages/SearchPage.js
@@ -1,13 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import ProfilePage from './pages/ProfilePage'
 
 import * as firebase from 'firebase';
 
 export default class SearchPage extends React.Component {
 
 
+    constructor(props){
+        super(props);
+    
+        if (!firebase.apps.length) { firebase.initializeApp(ApiKeys.FirebaseConfig); }
+    
+        
+      }
 
  render() {
      return (
